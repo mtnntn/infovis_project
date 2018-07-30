dc9c9d0616d155bf555493c51ca04cf2bc9af188
mtnntn <a.matinata@gmail.com>
2018-07-31 01:20:27 +0200

    Zone Information on MouseOver

--- infovis/js/colormaps.js
+++ infovis/js/colormaps.js
@@ -9,7 +9,6 @@ let borough_color_switcher = {
     "Queens" : d3.rgb(248, 189, 11, 0.78),
     "Staten Island" : d3.rgb(22, 22, 212, 0.69),
     "EWR" : d3.rgb(155, 0, 198, 0.55),
-
 };
 
 Promise.all([d3.json("data/geodata/ny_zones.json")]).then(plot_map);
@@ -32,6 +31,8 @@ function plot_map(us) {
         .enter()
         .append("path")
         .attr("class", "zone")
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handledMouseOut)
         .attr("location_id", function (d) { return d.properties["LocationID"] })
         .attr("borough", function (d) { return d.properties["borough"] })
         .attr("zone", function (d) { return d.properties["zone"] })
@@ -39,5 +40,38 @@ function plot_map(us) {
             return borough_color_switcher[d.properties["borough"]]
         })
         .attr("d", path)
-        .append("title").text(function(d){ let zi = d.properties; console.log(zi); return zi["LocationID"]+ " - " +zi["borough"] + " - " + zi["zone"]; })
+}
+
+function handleMouseOver(d, i) {  // Add interactivity
+    let location_id = d3.select(this).attr("location_id");
+    let zone = d3.select(this).attr("zone");
+    let borough = d3.select(this).attr("borough");
+
+    let infobox = svg.append("g").attr("transform", "translate(200,200)").classed("infobox", true);
+
+    infobox.append("circle")
+        .attr("r", 160)
+        .attr("fill", borough_color_switcher[borough]);
+
+    infobox.append("text").attr("text-anchor", "middle")
+        .text(location_id)
+        .attr("dy", -20)
+        .attr("fill", "white")
+        .attr("font-size", "1.5em");
+
+    infobox.append("text").attr("text-anchor", "middle")
+        .text(zone)
+        .attr("dy", 10)
+        .attr("fill", "white")
+        .attr("font-size", "1.2em");
+
+    infobox.append("text").attr("text-anchor", "middle")
+        .text(borough)
+        .attr("dy", 40)
+        .attr("fill", "white")
+        .attr("font-size", "1.2em");
+}
+
+function handledMouseOut(d, i) {
+    svg.selectAll("g.infobox").remove();
 }

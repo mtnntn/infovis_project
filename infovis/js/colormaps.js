fad366398e621cb9ca277b0f0676cc65d4b9832d
mtnntn <a.matinata@gmail.com>
2018-08-04 13:21:55 +0200

    Baseline for colormap

--- infovis/js/colormaps.js
+++ infovis/js/colormaps.js
@@ -13,8 +13,47 @@ d3.select("#form_submit").on("click", function(){
     let ts = d3.select("#ts").node().value;
 
     console.log("Submitted values:", month, dow, ts, prop);
+    // Month	DayOfWeek	TimeSlot	PULocationID	DOLocationID	Frequency
+
     Promise.all([d3.tsv("data/month_dow_ts_puloc_doloc.csv")]).then(function (data) {
+
         data = data[0];
-        console.log("Data loaded");
+
+        let result = [];
+        let max = 0;
+
+        console.log("Computing data...");
+        data.forEach(function(trip){
+
+            if(month === "-1")
+                delete trip["Month"];
+            else if (month !== trip["Month"].toString())
+                return;
+            if(dow === "-1")
+                delete trip["DayOfWeek"];
+            else if (dow !== trip["DayOfWeek"].toString())
+                return;
+            if(ts === "-1")
+                delete trip["TimeSlot"];
+            else if (ts !== trip["TimeSlot"].toString())
+                return;
+
+            let current_id = (prop === "pull") ? trip["DOLocationID"] : trip["PULocationID"] ;
+
+            if (result[current_id] == null || result[current_id] === undefined)
+                result[current_id] = 0;
+
+            result[current_id] += parseInt(trip["Frequency"]);
+
+            if(result[current_id] > max)
+                max = parseInt(result[current_id]);
+        });
+
+        console.log("Recoloring...")
+        svg.selectAll(".zone").attr("fill", function(node, index, nodes){
+            let location_id = node["properties"]["LocationID"];
+            return d3.interpolateViridis(result[location_id]/max);
+        });
+        console.log("Done.");
     });
 });

95dc28c51c111dbe54de74e91cfab0cf3be5e927
mtnntn <a.matinata@gmail.com>
2018-08-05 18:34:23 +0200

    Colormaps improved style and performances

--- infovis/js/colormaps.js
+++ infovis/js/colormaps.js
@@ -1,59 +1,60 @@
-let svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+let svg = d3.select("svg");
+let width = +svg.attr("width");
+let height = +svg.attr("height");
 
-Promise.all([d3.json("data/geodata/ny_zones.json")]).then(function (data) {
-    plot_map(svg, data);
-});
+/* Form fields */
+let month = d3.select("#month");
+let prop = d3.select("#property");
+let dow = d3.select("#dow");
+let ts = d3.select("#ts");
 
-d3.select("#form_submit").on("click", function(){
-    let month = d3.select("#month").node().value;
-    let prop = d3.select("#property").node().value;
-    let dow = d3.select("#dow").node().value;
-    let ts = d3.select("#ts").node().value;
+Promise.all([d3.json("data/geodata/ny_zones.json")]).then(function (map_data) {
 
-    console.log("Submitted values:", month, dow, ts, prop);
-    // Month	DayOfWeek	TimeSlot	PULocationID	DOLocationID	Frequency
+    let excluded_zones = [];
 
-    Promise.all([d3.tsv("data/month_dow_ts_puloc_doloc.csv")]).then(function (data) {
+    d3.select("#month").attr("value", "-1");
+    d3.select("#dow").attr("value", "-1");
+    d3.select("#ts").attr("value", "-1");
+    d3.select("#property").attr("value", "pull");
 
-        data = data[0];
+    Promise.all([d3.tsv("data/month_dow_ts_puloc_doloc.csv")]).then(function(dataset_data){
 
-        let result = [];
-        let max = 0;
+        plot_map(svg, map_data[0], function(){return "black";} );
 
-        console.log("Computing data...");
-        data.forEach(function(trip){
+        recolor_map(svg, dataset_data[0], excluded_zones);
 
-            if(month === "-1")
-                delete trip["Month"];
-            else if (month !== trip["Month"].toString())
-                return;
-            if(dow === "-1")
-                delete trip["DayOfWeek"];
-            else if (dow !== trip["DayOfWeek"].toString())
-                return;
-            if(ts === "-1")
-                delete trip["TimeSlot"];
-            else if (ts !== trip["TimeSlot"].toString())
-                return;
-
-            let current_id = (prop === "pull") ? trip["DOLocationID"] : trip["PULocationID"] ;
-
-            if (result[current_id] == null || result[current_id] === undefined)
-                result[current_id] = 0;
-
-            result[current_id] += parseInt(trip["Frequency"]);
-
-            if(result[current_id] > max)
-                max = parseInt(result[current_id]);
+        svg.selectAll(".zone").on("click", function(n){
+            console.log("Updating Excluded zones: ", excluded_zones);
+            let zone_location_id = n.properties["LocationID"];
+            excluded_zones = exclude_zone(zone_location_id, excluded_zones);
+            console.log("Excluded zones updating done: ", excluded_zones);
         });
 
-        console.log("Recoloring...")
-        svg.selectAll(".zone").attr("fill", function(node, index, nodes){
-            let location_id = node["properties"]["LocationID"];
-            return d3.interpolateViridis(result[location_id]/max);
+        d3.select("#form_submit").on("click", function(){ // recolor map according to the submitted input.
+            recolor_map(svg, dataset_data[0], excluded_zones);
         });
-        console.log("Done.");
+
     });
 });
+
+function recolor_map(svg, dataset_data, excluded_zones){
+    let month_v = month.node().value;
+    let prop_v = prop.node().value;
+    let dow_v = dow.node().value;
+    let ts_v = ts.node().value;
+    console.log("Submitted values:", month_v, dow_v, ts_v, prop_v, excluded_zones);
+    colorize_map(svg, dataset_data, month_v, dow_v, ts_v, prop_v, excluded_zones);
+    console.log("Done...");
+};
+
+function exclude_zone(zone_to_exclude, excluded_zones){
+    let zone_to_exclude_index = excluded_zones.indexOf(zone_to_exclude);
+    if(zone_to_exclude_index === -1){ //the zone should be added to the list
+
+        excluded_zones.push(zone_to_exclude);
+    }
+    else{
+        excluded_zones.splice(zone_to_exclude_index, 1);
+    }
+    return excluded_zones;
+}
--- infovis/js/colormaps_utils.js
+++ infovis/js/colormaps_utils.js
@@ -7,15 +7,17 @@ var borough_color_switcher = {
     "EWR" : d3.rgb(155, 0, 198, 0.55),
 };
 
-function plot_map(svg, data) {
-    data = data[0];
+function default_coloring(zone_data){
+    return borough_color_switcher[zone_data.properties["borough"]]
+}
 
+function plot_map(svg, data, recoloring_function=default_coloring) {
+    console.log("Plotting data...");
     let ny_zones = topojson.feature(data, {
         type: "GeometryCollection",
         geometries: data.objects["ny_zones"]["geometries"]
     });
-    // Reference: http://mapshaper.org/, https://github.com/topojson/topojson/wiki/Introduction, https://github.com/d3/d3-geo
-    let projections = d3.geoAlbers().fitExtent( [ [0,0], [1260,720] ], ny_zones);
+    let projections = d3.geoMercator().center([-73.94, 40.70]).scale(90000).translate([ (1260)/2, 900/2]);
     let path = d3.geoPath(projections);
 
     svg.append("g")
@@ -25,11 +27,83 @@ function plot_map(svg, data) {
         .enter()
         .append("path")
         .attr("class", "zone")
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handledMouseOut)
         .attr("location_id", function (d) { return d.properties["LocationID"] })
         .attr("borough", function (d) { return d.properties["borough"] })
         .attr("zone", function (d) { return d.properties["zone"] })
-        .attr("fill", function(d) {
-            return borough_color_switcher[d.properties["borough"]]
-        })
-        .attr("d", path)
+        .attr("fill", recoloring_function)
+        .attr("d", path);
+    console.log("Done plotting...");
 }
+
+function handleMouseOver() {  // Add interactivity
+    let infobox_div = d3.select("div.infobox");
+    let selected_element = d3.select(this);
+
+    let location_id = selected_element.attr("location_id");
+    let zone = selected_element.attr("zone");
+    let borough = selected_element.attr("borough");
+    let color = borough_color_switcher[borough];
+
+    infobox_div.style("color", color);
+    infobox_div.select(".zone_location_id").append("text").text(location_id);
+    infobox_div.select(".zone_name").append("text").text(zone);
+    infobox_div.select(".zone_borough").append("text").text(borough.toUpperCase());
+}
+
+function handledMouseOut() {
+    let infobox_div = d3.select("div.infobox");
+    infobox_div.selectAll("br").remove();
+    infobox_div.selectAll("text").remove();
+}
+
+let colorize_map = function (svg, data, month, dow, ts, prop, excluded=[]) {
+
+    let result = [];
+    let max = 0;
+
+    console.log("Computing data...");
+    data.forEach(function(trip){
+
+        let current_id = (prop === "pull") ? trip["PULocationID"] : trip["DOLocationID"] ;
+
+        if (excluded.indexOf(current_id) === -1) { //zone not excluded
+
+            if(month !== "-1" && month !== trip["Month"].toString())
+                return;
+            if(dow !== "-1" && dow !== trip["DayOfWeek"].toString())
+                return;
+            if(ts !== "-1" && ts !== trip["TimeSlot"].toString())
+                return;
+
+            if (result[current_id] == null || result[current_id] === undefined)
+                result[current_id] = 0;
+
+            result[current_id] += parseInt(trip["Frequency"]);
+
+            if(result[current_id] > max)
+                max = parseInt(result[current_id]);
+        }
+    });
+
+    console.log("Recoloring...");
+
+    svg.selectAll(".zone").attr("fill", function(node){
+        let location_id = node["properties"]["LocationID"];
+        if(excluded.indexOf(location_id) === -1){ // zone not excluded
+            let value_to_iterpolate = result[location_id]/max;
+            if(prop==="pull")
+                return d3.interpolateRdYlGn(value_to_iterpolate);
+                // return d3.interpolateOranges(value_to_iterpolate);
+            return d3.interpolateRdYlBu(value_to_iterpolate);
+            // return d3.interpolatePurples(value_to_iterpolate);
+        }
+        else{
+            return "black";
+        }
+    });
+
+    console.log("Recoloring done...");
+
+};
